ade508b676cda3b4c1e16c3c8509b5c34c521ef6
Harsh Sarin <harsh.sarin@here.com>
2018-12-13 12:04:18 -0600

    Project setup.

---
+++ src/main/java/com/sarin/springtest/springpet_clinic_app/SpringPetClinicAppApplication.java
@@ -0,0 +1,13 @@
+package com.sarin.springtest.springpet_clinic_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SpringPetClinicAppApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SpringPetClinicAppApplication.class, args);
+   }
+
+}
+
