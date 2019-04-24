cd77876acb056e9cb5feaefb1231f5465cb29e87
sarin <harsh.sarin@here.com>
2019-04-24 16:26:24 -0500

    Added Visit and Speciality entities.

--- pet-clinic-data/src/main/java/com/sarin/springtest/springpet_clinic_app/model/Owner.java
+++ pet-clinic-data/src/main/java/com/sarin/springtest/springpet_clinic_app/model/Owner.java
@@ -1,4 +1,16 @@
 package com.sarin.springtest.springpet_clinic_app.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
